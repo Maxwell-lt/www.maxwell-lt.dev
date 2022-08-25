e354c53e7176210732e5c27332a417a91ec4231a
Maxwell L-T <maxwell.lt@live.com>
2022-08-25 08:04:10 -0400

    Embed static files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 #[macro_use]
 extern crate rocket;
-use rocket::fs::FileServer;
+use std::{path::PathBuf, borrow::Cow, ffi::OsStr};
 
-mod env;
+use rocket::http::ContentType;
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "frontend/build"]
+struct Asset;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/<file..>")]
+fn static_files(mut file: PathBuf) -> Option<(ContentType, Cow<'static, [u8]>)> {
+    let filename = file.display().to_string();
+    let asset = match Asset::get(&filename) {
+        Some(data) => data,
+        None => {
+            file.push("index.html");
+            let index_filename = file.display().to_string();
+            Asset::get(&index_filename)?
+        }
+    };
+    let content_type = file
+        .extension()
+        .and_then(OsStr::to_str)
+        .and_then(ContentType::from_extension)
+        .unwrap_or(ContentType::Bytes);
+
+    Some((content_type, asset.data))
+}
+
 #[launch]
 fn rocket() -> _ {
-    let env = crate::env::parse_args();
     rocket::build()
         .mount("/api", routes![index])
-        .mount("/", FileServer::from(env.frontend_files_location))
+        .mount("/", routes![static_files])
 }
