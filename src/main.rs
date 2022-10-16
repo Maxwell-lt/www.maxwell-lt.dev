7ff6cb7a3f846494ea894e257c0213595f38e4c5
Maxwell L-T <maxwell.lt@live.com>
2022-10-16 10:07:54 -0400

    Start work on building node and rust code separately

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,18 @@
 #[macro_use]
 extern crate rocket;
-use std::{path::PathBuf, borrow::Cow, ffi::OsStr};
+use rocket::fs::FileServer;
 
-use rocket::http::ContentType;
-use rust_embed::RustEmbed;
-
-#[derive(RustEmbed)]
-#[folder = "frontend/build"]
-struct Asset;
+mod env;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/<file..>")]
-fn static_files(mut file: PathBuf) -> Option<(ContentType, Cow<'static, [u8]>)> {
-    let filename = file.display().to_string();
-    let asset = match Asset::get(&filename) {
-        Some(data) => data,
-        None => {
-            file.push("index.html");
-            let index_filename = file.display().to_string();
-            Asset::get(&index_filename)?
-        }
-    };
-    let content_type = file
-        .extension()
-        .and_then(OsStr::to_str)
-        .and_then(ContentType::from_extension)
-        .unwrap_or(ContentType::Bytes);
-
-    Some((content_type, asset.data))
-}
-
 #[launch]
 fn rocket() -> _ {
+    let env = crate::env::parse_args();
     rocket::build()
         .mount("/api", routes![index])
-        .mount("/", routes![static_files])
+        .mount("/", FileServer::from(env.frontend_files_location))
 }
